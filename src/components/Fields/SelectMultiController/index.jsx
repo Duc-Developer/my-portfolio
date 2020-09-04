7c945447ef3b44e1b59b0a88a556a0ba5f72dd93
DucJS <mrtranduc1994@outlook.com>
2020-09-04 21:24:07 +0700

    create default SelectField

---
+++ src/components/Fields/SelectMultiController/index.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Controller } from 'react-hook-form';
+import { Label, Input, Row, Badge, Col, Container } from 'reactstrap';
+
+SelectMultiController.propTypes = {
+    errors: PropTypes.object,
+    control: PropTypes.object,
+    name: PropTypes.string,
+    id: PropTypes.string,
+    lableName: PropTypes.string,
+    listSelect: PropTypes.array,
+    rules: PropTypes.object
+}
+
+SelectMultiController.defaultProps = {
+    errors: {},
+    rules: {},
+    listSelect: ["Select..."]
+}
+
+export default function SelectMultiController(props) {
+
+    const {
+        errors,
+        control,
+        name,
+        id,
+        lableName,
+        listSelect,
+        rules
+    } = props;
+
+    return <div className="select-multi-controller">
+        <Container>
+            <Row>
+                <Col xs="12" lg="4">
+                    <Label for={id}>
+                        <b>{lableName}</b>
+                    </Label>
+                </Col>
+                <Col xs="12" lg="8">
+                    {errors[name] && <Badge color="warning">
+                        <i>{errors[name].message}</i>
+                    </Badge>}
+                </Col>
+            </Row>
+            <Controller
+                control={control}
+                rules={rules}
+                name={name}
+                defaultValue={control.defaultValuesRef.current[name]}
+                render={({ onChange, value }) => {
+                    return <div>
+                        <Input type="select" defaultValue={value} id={id}>
+                            {
+                                listSelect.map((item, index) => {
+                                    return <option
+                                        key={index}
+                                        value={item}>
+                                        {item}
+                                    </option>
+                                })
+                            }
+                        </Input>
+                    </div>
+                }}
+            />
+        </Container>
+    </div>
+}
