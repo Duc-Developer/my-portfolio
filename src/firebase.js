a05a658a838bbffe76d38156c052c71171f4151e
DucJS <mrtranduc1994@outlook.com>
2020-09-09 20:08:02 +0700

    add env but it fail

--- src/firebase.js
+++ src/firebase.js
@@ -3,6 +3,16 @@ import "firebase/analytics";
 import "firebase/auth";
 import "firebase/firestore";
 
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//     databaseURL: process.env.REACT_APP_FIREBASE_DATABSE_URL,
+//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+//     appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+// };
 const firebaseConfig = {
     apiKey: "AIzaSyCl6Yjaj9saTrCgeNUCeqPtmoHscvHwvZ0",
     authDomain: "my-portfolio-f7109.firebaseapp.com",
@@ -12,7 +22,7 @@ const firebaseConfig = {
     messagingSenderId: "239023323515",
     appId: "1:239023323515:web:5b20b412ca6318afe565de",
     measurementId: "G-CQGJNWSX8H"
-};
+  };
 
 const app = firebase.initializeApp(firebaseConfig)
 const database = firebase.database()
